702d299e5faf00f722ba6b450813b602c4812197
IweZix <IweZix@users.noreply.github.com>
2024-06-24 21:22:06 +0200

    add: guildMemberUpdate event

---
+++ src/events/bot/guildMemberUpdate.ts
@@ -0,0 +1,82 @@
+import {
+    Events,
+    EmbedBuilder,
+    TextChannel,
+    GuildMember,
+} from 'discord.js';
+
+import { parse } from '@/utils/json';
+import { CustomClient } from '@/base/classes/CustomClient';
+
+const jsonPath = './data/log.json';
+
+const embed = new EmbedBuilder()
+    .setTitle('Member Update')
+    .setColor('#00FF00')
+    .setTimestamp();
+
+module.exports = {
+    name: Events.GuildMemberUpdate,
+
+    async run(client: CustomClient, oldMember: GuildMember, newMember: GuildMember) {
+        
+        const guildId = newMember.guild?.id;
+        const data = parse(jsonPath, []);
+        const guildData = data.find((data: any) => data.guildId === guildId);
+
+        if (!guildData) {
+            return;
+        }
+
+        const logChannel = newMember.guild?.channels.cache.get(
+            guildData.logChannelId
+        ) as TextChannel;
+
+        if (!logChannel) {
+            return;
+        }
+
+        let changes = '';
+
+        if (oldMember.nickname !== newMember.nickname) {
+            changes += `**Nickname:** ${oldMember.nickname} -> ${newMember.nickname}\n`;
+        }
+
+        if (oldMember.roles.cache.size !== newMember.roles.cache.size) {
+            const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+            const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+
+            if (addedRoles.size > 0) {
+                changes += `**Roles Added:** ${addedRoles.map(role => role.name).join(', ')}\n`;
+            }
+
+            if (removedRoles.size > 0) {
+                changes += `**Roles Removed:** ${removedRoles.map(role => role.name).join(', ')}\n`;
+            }
+        }
+
+        if (oldMember.user.username !== newMember.user.username) {
+            changes += `**Username:** ${oldMember.user.username} -> ${newMember.user.username}\n`;
+        }
+
+        if (oldMember.user.discriminator !== newMember.user.discriminator) {
+            changes += `**Discriminator:** ${oldMember.user.discriminator} -> ${newMember.user.discriminator}\n`;
+        }
+
+        if (oldMember.user.avatar !== newMember.user.avatar) {
+            changes += `**Avatar:** ${oldMember.user.avatar} -> ${newMember.user.avatar}\n`;
+        }
+
+        embed.setDescription(`
+            A member has been updated.
+            > **Member:** <@${newMember.id}>
+            
+            __**Changes:**__
+            ${changes}
+        `).setThumbnail(newMember.user.displayAvatarURL());
+
+
+
+        return await logChannel.send({ embeds: [embed] });
+    }
+};
