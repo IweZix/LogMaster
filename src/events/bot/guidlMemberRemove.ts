8ce1f1ac02883144a59405ab457d6a5b7b70269b
IweZix <IweZix@users.noreply.github.com>
2024-06-24 20:20:49 +0200

    add: guildMemberRemove event

---
+++ src/events/bot/guidlMemberRemove.ts
@@ -0,0 +1,45 @@
+import {
+    Events,
+    EmbedBuilder,
+    TextChannel,
+    GuildMember,
+} from 'discord.js';
+
+import { parse } from '@/utils/json';
+import { CustomClient } from '@/base/classes/CustomClient';
+
+const jsonPath = './data/log.json';
+
+const embed = new EmbedBuilder()
+    .setTitle('Member Left')
+    .setColor('#00FF00')
+    .setTimestamp();
+
+module.exports = {
+    name: Events.GuildMemberRemove,
+
+    async run(client: CustomClient, member: GuildMember) {
+        const guildId = member.guild?.id;
+        const data = parse(jsonPath, []);
+        const guildData = data.find((data: any) => data.guildId === guildId);
+
+        if (!guildData) {
+            return;
+        }
+
+        const logChannel = member.guild?.channels.cache.get(
+            guildData.logChannelId
+        ) as TextChannel;
+
+        if (!logChannel) {
+            return;
+        }
+            
+        embed.setDescription(`
+            A member has left the server.
+            > **name:** <#${member.id}>
+        `).setThumbnail(member.user.displayAvatarURL());
+
+        return await logChannel.send({ embeds: [embed] });
+    }
+};
