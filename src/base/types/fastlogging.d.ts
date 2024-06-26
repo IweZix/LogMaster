4046d86bd0bf2015ef4b2fe6e0ab276a34a0a0db
IweZix <IweZix@users.noreply.github.com>
2024-06-26 20:29:50 +0200

    update: fastlogging

--- src/base/types/fastlogging.d.ts
+++ src/base/types/fastlogging.d.ts
@@ -2,13 +2,19 @@ declare module 'fastlogging' {
     interface FastLogging {
         info(message: string): void;
         success(message: string): void;
-        // Implémentez d'autres méthodes au besoin
+        log(message: string): void;
+        warn(message: string): void;
+        error(message: string): void;
+        debug(message: string): void;
     }
 
     export default class implements FastLogging {
         constructor(debug?: boolean, toFile?: boolean);
         info(message: string): void;
         success(message: string): void;
-        // Implémentez d'autres méthodes au besoin
+        log(message: string): void;
+        warn(message: string): void;
+        error(message: string): void;
+        debug(message: string): void;
     }
 }
